54bc840434d78d43ded4a3c1edb73a46dc04f7dd
lunnaboo <lunnaboo@outlook.com>
2025-10-21 00:48:31 -0300

    done

---
+++ Libft_Tester/test_utils/test_utils.h
@@ -0,0 +1,12 @@
+#ifndef TEST_UTILS_H
+# define TEST_UTILS_H
+
+# include <stdio.h>
+# include <string.h>
+# include <stdlib.h>
+
+// global counters
+extern int g_tests_run;
+extern int g_tests_passed;
+
+#endif
